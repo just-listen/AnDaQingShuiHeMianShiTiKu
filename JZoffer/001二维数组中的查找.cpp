094d51a6c6f7b51e78e7736df753d74090995d9c
just-listen <18756926806@163.com>
2021-07-19 11:32:38 +0800

    first commit

---
+++ JZoffer/001二维数组中的查找.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        int m = array.size();
+        if(m == 0) return false;
+        int n = array[0].size();
+        int row = 0, col = n - 1;
+        while(row < m && col >= 0){
+            if(target < array[row][col]) col--;
+            else if(target > array[row][col]) row++;
+            else return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+    int target = 7;
+    vector<vector<int>> vec({{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}});
+    Solution s;
+    cout << "answer: " << s.Find(target, vec) << endl;
+    return 0;
+}
